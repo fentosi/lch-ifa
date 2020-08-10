d57568860074385435ac0e98160373561dfdf0a2
Adam Fentosi <fentosi.adam@gmail.com>
2020-08-10 08:12:38 +0200

    feat(countries): Add European countries list

---
+++ src/includes/Countries.php
@@ -0,0 +1,34 @@
+<?php
+
+class Countries {
+    const EU = [
+        'AT' => ['name' => 'Austria'],
+        'BE' => ['name' => 'Belgium'],
+        'BG' => ['name' => 'Bulgaria'],
+        'CY' => ['name' => 'Cyprus'],
+        'CZ' => ['name' => 'Czech Republic'],
+        'DE' => ['name' => 'Germany'],
+        'DK' => ['name' => 'Denmark'],
+        'EE' => ['name' => 'Estonia'],
+        'ES' => ['name' => 'Spain'],
+        'FI' => ['name' => 'Finland'],
+        'FR' => ['name' => 'France'],
+        'GB' => ['name' => 'United Kingdom'],
+        'GR' => ['name' => 'Greece'],
+        'HU' => ['name' => 'Magyar'],
+        'HR' => ['name' => 'Croatia'],
+        'IE' => ['name' => 'Ireland, Republic of (EIRE)'],
+        'IT' => ['name' => 'Italy'],
+        'LT' => ['name' => 'Lithuania'],
+        'LU' => ['name' => 'Luxembourg'],
+        'LV' => ['name' => 'Latvia'],
+        'MT' => ['name' => 'Malta'],
+        'NL' => ['name' => 'Netherlands'],
+        'PL' => ['name' => 'Poland'],
+        'PT' => ['name' => 'Portugal'],
+        'RO' => ['name' => 'Romania'],
+        'SE' => ['name' => 'Sweden'],
+        'SI' => ['name' => 'Slovenia'],
+        'SK' => ['name' => 'Slovakia']
+    ];
+}
