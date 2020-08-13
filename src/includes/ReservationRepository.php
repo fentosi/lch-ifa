3201b9bfd0570ea9c709bb2194a77c6af59072fe
Adam Fentosi <fentosi.adam@gmail.com>
2020-08-13 20:59:43 +0200

    feat(reservation-repository): New repository

---
+++ src/includes/ReservationRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+
+class ReservationRepository
+{
+    /**
+     * @var mysqli
+     */
+    private $mysqli;
+
+    public function __construct(mysqli $mysqli)
+    {
+        $this->mysqli = $mysqli;
+    }
+
+    public function getById(int $id)
+    {
+        if (!($statement = $this->mysqli->prepare(
+            "
+            SELECT
+                id, reservation_hash, debit_hash, room_hash, status
+            FROM 
+                ifa_reservation
+            WHERE 
+                id = ?"))) {
+            throw new Exception("SQL Statement error");
+        }
+
+        $statement->bind_param('d', $id);
+
+        if (!$statement->execute()) {
+            throw new Exception("Execute failed");
+        }
+
+        if (!($result = $statement->get_result())) {
+            throw new Error("Getting result set failed");
+        }
+
+        return $result->fetch_assoc();
+    }
+}
