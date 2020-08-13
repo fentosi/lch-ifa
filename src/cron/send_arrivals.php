2416606630d6920307928ebcac52e0664484436d
Adam Fentosi <fentosi.adam@gmail.com>
2020-08-13 21:00:23 +0200

    feat(cron): send arrivals script

---
+++ src/cron/send_arrivals.php
@@ -0,0 +1,41 @@
+<?php
+
+ini_set('display_errors', 'on');
+require_once ('../vendor/autoload.php');
+
+$dotenv = Dotenv\Dotenv::create('../');
+$dotenv->load();
+
+require_once ('../includes/dbConnection.php');
+require_once('../includes/entities/Contact.php');
+require_once ('../includes/ContactRepository.php');
+require_once ('../includes/ReservationRepository.php');
+require_once('../includes/entities/Reservation.php');
+require_once ('../includes/ReservationStatuses.php');
+require_once ('../includes/FelhoMatracClient.php');
+
+$contactRepository = new ContactRepository($mysqli);
+$reservationRepository = new ReservationRepository($mysqli);
+$felhoMatracClient = new FelhoMatracClient($_ENV['FELHOMATRAC_CUSTOMER'], $_ENV['FELHOMATRAC_TOKEN']);
+
+$errors = [];
+
+try {
+    $prevDay = (new DateTime())->modify('-1 day');
+    $contacts = $contactRepository->getAllWithReservationDataByStatus(ReservationStatuses::STATUS_CODES[ReservationStatuses::CLAIMED]);
+    $arrivedContacts = array_filter($contacts, function($contact) use ($prevDay) {
+        return $contact['arrival_date'] === $prevDay->format('Y-m-d');
+    });
+
+    foreach ($arrivedContacts as $contact) {
+        $contact = Contact::createFrom($contact);
+        $reservation = Reservation::createFrom($mysqli, $reservationRepository->getById($contact->getReservationId()));
+        $reservation->setStatus(ReservationStatuses::STATUS_CODES[ReservationStatuses::ARRIVED])->save();
+        $reservation = $felhoMatracClient->makeReservation($reservation, [$contact]);
+        $reservation->save();
+    }
+} catch (Exception $e) {
+    $errors[] = $e->getMessage();
+}
+
+print_r($errors);
